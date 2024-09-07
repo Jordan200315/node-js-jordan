469bc458bebc146a2b803dc1ca7be9363d955efd
Jordan200315 <jordanmalibiran406@gmail.com>
2024-09-07 13:32:42 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/about', (req, res) => res.render('about'));
+app.get('/services', (req, res) => res.render('services'));
+app.get('/contact', (req, res) => res.render('contact'));
+app.get('/feedback', (req, res) => res.render('feedback'));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
